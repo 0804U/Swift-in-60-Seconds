64f4e0191e7ecfc5193663cec94d425ca05207a8
Steve Graff <slgraff@gmail.com>
2018-04-04 17:05:22 -0400

    Final code for Chapter 3

--- Chapter-3-Operators_and_conditions.playground/Contents.swift
+++ Chapter-3-Operators_and_conditions.playground/Contents.swift
@@ -19,4 +19,95 @@ let remainder = 13 % secondScore
 
 
 // Operator overloading
+let meaningOfLife = 42
+let doubleMeaning = 42 + 42
+
+let fakers = "Fakers gonna "
+let action = fakers + "fake"
+
+let firstHalf = ["John", "Paul"]
+let secondHalf = ["George", "Ringo"]
+let beatles = firstHalf + secondHalf
+
+
+// Compound assignment operators
+var score = 95
+score -= 5
+
+var quote = "The rain in Spain falls mainly on the "
+quote += "Spaniards"
+
+
+// Comparison operators
+let firstScore2 = 6
+let secondScore2 = 4
+
+firstScore == secondScore
+firstScore != secondScore
+
+firstScore < secondScore
+firstScore >= secondScore
+
+"Taylor" <= "Swift"
+
+
+// Conditions
+let firstCard = 11
+let secondCard = 10
+
+if firstCard + secondCard == 21 {
+    print("Blackjack!")
+}
+
+if firstCard + secondCard == 21 {
+    print("Blackjack!")
+} else {
+    print("Regular cards")
+}
+
+if firstCard + secondCard == 2 {
+    print("Aces - lucky!")
+} else if firstCard + secondCard == 21 {
+    print("Blackjack!")
+} else {
+    print("Regular cards")
+}
+
+
+// The ternary operator
+// let firstCard = 11
+// let secondCard = 10
+
+print(firstCard == secondCard ? "Cards are the same" : "Cards are different")
+
+
+// Switch statetments
+let weather = "sunny"
+
+switch weather {
+case "rain":
+    print("Bring an umbrella")
+case "snow":
+    print("Wrap up warm")
+case "sunny":
+    print("Wear sunscren")
+    fallthrough
+default:
+    print("Enjoy your day!")
+}
+
+
+// Range operators
+let score2 = 85
+
+switch score2 {
+case 0..<50:
+    print("You failed badly.")
+case 50..<85:
+    print("You did OK.")
+default:
+    print("You did great!")
+}
+
+
 
