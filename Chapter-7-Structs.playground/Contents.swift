828ca43be43554e2c8dde8184dc831073d74e622
Steve Graff <slgraff@gmail.com>
2018-04-24 21:36:54 -0400

    Add playground for Chapter 7 - Structs

--- Chapter-7-Structs.playground/Contents.swift
+++ Chapter-7-Structs.playground/Contents.swift
@@ -1,5 +1,164 @@
-//: Playground - noun: a place where people can play
+// Swift in 60 Seconds
+// Chapter 7 - Structs
+// https://www.hackingwithswift.com/sixty
 
 import UIKit
 
-var str = "Hello, playground"
+// Creating your own structs
+struct Sport {
+    var name: String
+}
+
+var tennis = Sport(name: "Tennis")
+print(tennis.name)
+
+tennis.name = "Lawn tennis"
+
+
+// Computed properties
+
+struct Sport2 {
+    var name: String
+    var isOlympicSport: Bool
+    
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic sport"
+        } else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
+}
+
+let chessBoxing = Sport2(name: "Chessboxing", isOlympicSport: false)
+print(chessBoxing.olympicStatus)
+
+
+// Property observers
+struct Progress {
+    var task: String
+    var amount: Int {
+        didSet {
+            print("\(task) is now \(amount)% complete")
+        }
+    }
+}
+
+var progress = Progress(task: "Loading data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
+
+
+// Methods
+struct City {
+    var population: Int
+    
+    func collectTaxes() -> Int {
+        return population * 1000
+    }
+}
+
+let london = City(population: 9_000_000)
+london.collectTaxes()
+
+
+// Mutating methods
+struct Person {
+    var name: String
+    
+    mutating func makeAnonymous() {
+        name = "Anonymous"
+    }
+}
+
+var person = Person(name: "Ed")
+person.makeAnonymous()
+
+
+// Properties and methods of strings
+let string = "Do or do not, there is no try."
+print(string.count)
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())
+
+
+// Initializers
+struct User {
+    var username: String
+    
+    init() {
+        username = "Anonymous"
+        print("Creating a new user!")
+    }
+}
+
+var user = User()
+user.username = "twostraws"
+
+
+// Referring to the current instance
+struct Person2 {
+    var name: String
+    
+    init(name: String) {
+        print("\(name) was born!")
+        self.name = name
+    }
+}
+
+
+// Lazy properties
+struct FamilyTree {
+    init() {
+        print("Creating family tree!")
+    }
+}
+
+struct Person3 {
+    var name: String
+    var familyTree = FamilyTree()
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+var ed = Person3(name: "Ed")
+// lazy var familyTree = FamilyTree() // error "Lazy is only valid for members of a struct or class"
+ed.familyTree
+
+
+// Static properties and methods
+struct Student {
+    static var classSize = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+        Student.classSize += 1
+    }
+}
+
+let ed2 = Student(name: "Ed")
+let taylor = Student(name: "Taylor")
+print(Student.classSize)
+
+
+// Access control
+struct Person4 {
+    private var id: String
+    
+    init(id: String) {
+        self.id = id
+    }
+    
+    func identity() -> String {
+        return "My social security number is \(id)"
+    }
+}
+
+let ed3 = Person4(id: "12345")
+
+
